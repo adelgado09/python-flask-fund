a20f8024c659c52ed26968e2db2ce16167f61c15
Ashton Delgado <56052219+adelgado09@users.noreply.github.com>
2022-11-24 20:38:18 -0800

    Update server.py
    added render templates  and app route INDEX

--- fundamentals/hello_flask/server.py
+++ fundamentals/hello_flask/server.py
@@ -1,8 +1,13 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template  # Import Flask to allow us to create our app and "render_template" links the html templates like css to html
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+    return "hello World!"  # Return the string 'Hello World!' as a response
+
+
+@app.route('/index')          # url + "/index"
+def index():
+    return render_template("index.html")  # Return the html page as a response
 
 
 # import statements, maybe some other routes
@@ -26,5 +31,6 @@ def show_user_profile(username, id):
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module
     app.run(debug=True)    # Run the app in debug mode.
+
 
 
